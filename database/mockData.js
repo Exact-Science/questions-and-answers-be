a7af10ba4bd22b296349340b82201c52ee83fb9d
Brian Bouchard <b.bouch@gmail.com>
2020-02-14 15:28:25 -0700

    install moment js and format date for database

--- database/mockData.js
+++ database/mockData.js
@@ -1,5 +1,6 @@
 var faker = require('faker');
 var fs = require('file-system');
+var Moment = require('moment');
 
 let arr = '';
 let questionsFilePath = '/Users/b-ghostvirus/Galvanize/Capstone-Project/SDC/database/data/additional_questions.csv';
@@ -9,11 +10,12 @@ for (let i=3521635; i < 3521700; i++){
   const id = i;
   const product_id = faker.random.number({min:1, max:1000000});
   const body = faker.lorem.sentence();
-  const date = faker.date.past();
+  let date = faker.date.past();
   const name = faker.random.word();
   const email = faker.internet.email();
   const reported = faker.random.number({min:0, max:1});
   const helpfulness = faker.random.number({min:0, max:50});
+  date = Moment(date).format().toString().substring(0,10);
   arr += `${id},${product_id},${body},${date},${name},${email},${reported},${helpfulness}\n`;
 }
 
