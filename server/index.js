f594d54d87667bc579ce0cf28e6d2314cc705e27
Brian Bouchard <b.bouch@gmail.com>
2020-02-14 10:30:08 -0700

    install postgres

--- server/index.js
+++ server/index.js
@@ -8,16 +8,4 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.post('/repos', (request, response) => {
-  db.save(request.body);
-})
-
-app.get('/repos', (request, response) => {
-  db.getAllRepos()
-    .then((results) => {
-      response.send(JSON.stringify(results))
-    })
-})
-
-
 app.listen(port, () => {console.log(`The server is up and running on port: ${port}`)});
