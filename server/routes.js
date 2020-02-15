f594a1357b0ff533d7aa1f0309b9d0fa666e88ae
Brian Bouchard <b.bouch@gmail.com>
2020-02-15 14:46:25 -0700

    stub out routes

--- server/routes.js
+++ server/routes.js
@@ -1,4 +1,39 @@
 const express = require('express');
-var router = express.Router();
+const router = express.Router();
+
+/* ------- Get ------- //
+// questions
+
+// answers
+
+
+/* ------- Post ------- //
+// question
+
+// answer
+
+/* ------- Put ------- //
+Helpful Question
+
+Report Question
+
+Helpful Answer
+
+Report Answer
+
+*/
+
+router.get('')
+// router.get('/user/:id', function (req, res, next) {
+//   // if the user ID is 0, skip to the next router
+//   if (req.params.id === '0') next('route')
+//   // otherwise pass control to the next middleware function in this stack
+//   else next()
+// }, function (req, res, next) {
+//   // render a regular page
+//   res.render('regular')
+// })
+
+
 
 module.exports = router;
