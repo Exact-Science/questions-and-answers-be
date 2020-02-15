f942f50df07a9caae158dfc3d80ec38f9b7eb0cb
Brian Bouchard <b.bouch@gmail.com>
2020-02-15 15:51:01 -0700

    define route framework without query dependencies

--- server/index.js
+++ server/index.js
@@ -3,22 +3,22 @@ const bodyParser = require('body-parser');
 const db = require('../database/index.js');
 // const cors = require('cors');
 const port = 3000;
-// const routes = require('./routes.js');
+const routes = require('./routes.js');
 
 const app = express();
 
 // app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/qa', function (req, res) {
-  // console.log(req.params.question_id)
-  console.log(req.body);
-  res.send("hello world");
-  // res.render('special')
-})
+// app.get('/qa', function (req, res) {
+//   // console.log(req.params.question_id)
+//   console.log(req.body);
+//   res.send("hello world");
+//   // res.render('special')
+// })
 
 
-// app.use('/', routes);
+app.use('/', routes);
 
 app.listen(port, () => {console.log(`The server is up and running on port: ${port}`)});
 
--- server/routes.js
+++ server/routes.js
@@ -3,30 +3,46 @@ const router = express.Router();
 
 /* ------- Get ------- */
 // questions
-
-// answers
-console.log("testing");
-
-router.get('qa/:question_id/answers', function (req, res) {
-  // console.log(req.params.question_id)
-  console.log(req.body);
-  res.send(req.body);
-  // res.render('special')
+router.get('/qa/:product_id', function (req, res) {
+  res.send(req.params.question_id);
 })
 
+// answers
+router.get('/qa/:question_id/answers', function (req, res) {
+  res.send(req.params.question_id);
+})
 
 /* ------- Post ------- */
 // question
+router.post('/qa/:product_id', function (req, res) {
+  res.send(req.params.question_id);
+})
 
 // answer
+router.post('/qa/:question_id/answers', function (req, res) {
+  res.send(req.params.question_id);
+})
+
 
 /* ------- Put ------- */
 //Helpful Question
+router.put('/qa/question/:question_id/helpful', function (req, res) {
+  res.send(req.params.question_id);
+})
 
 //Report Question
+router.put('/qa/question/:question_id/report', function (req, res) {
+  res.send(req.params.question_id);
+})
 
 //Helpful Answer
+router.put('/qa/answer/:answer_id/helpful', function (req, res) {
+  res.send(req.params.question_id);
+})
 
 //Report Answer
+router.put('PUT /qa/answer/:answer_id/report', function (req, res) {
+  res.send(req.params.question_id);
+})
 
 module.exports = router;
