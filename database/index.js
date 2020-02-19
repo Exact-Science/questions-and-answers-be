e2356044ed98a9defa518af63aec3e2f0810603e
Brian Bouchard <b.bouch@gmail.com>
2020-02-19 09:23:35 -0700

    add db name to pg pool

--- database/index.js
+++ database/index.js
@@ -6,6 +6,7 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE,
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
--- database/queries/questions/questions.js
+++ database/queries/questions/questions.js
@@ -3,7 +3,7 @@ const db = require('../../index.js');
 const getProductQuestions = (product_id) => {
   // const query = `select questions.questionId, questions.questionBody, questions.date, questions.questionName, questions.questionEmail, questions.questionReported, questions.questionHelpfulness, answers.answerId, answers.answerBody, answers.answerDate, answers.answerName, answers.answerEmail, answers.answerReported, answers.answerHelpfulness, photos.photoId, photos.photoUrl from questions left join answers on questions.id = answers.question_id left join photos on answers.id = photos.answer_id where questions.productId = '${product_id}' order by questions.questionHelpfulness desc`;
   // const query = `select questions.quesstion_id from questions left join answers on questions.id = answers.id left join photos on answers.id = photos.answer_id where questions.product_id = '${product_id}' order by questions.helpfulness desc`;
-  const query = `select questions.question_id from questions where product_id = '${product_id}' limit 10`;
+  const query = `select * from questions limit 10`;
   return db.pool.query(query)
   .then((results) => results.rows);
 }
